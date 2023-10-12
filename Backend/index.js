dd2545c37b3efd3dd04f5eb910d515b8fd972b0f
Bhumika Vasanta <bhumika.vasanta@quantiphi.com>
2023-10-12 23:10:20 +0530

    Changes Done

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
+const Comment = require('./models/Comment');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
@@ -10,14 +11,10 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
-const path = require('path');
-
-// const PORT = process.env.PORT || 4000
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(express.static(path.join(__dirname + "/public")))
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
@@ -69,17 +66,12 @@ app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
-app.post('/post', async (req,res) => {
-  // const {originalname,path} = req.file;
-  // const parts = originalname.split('.');
-  // const ext = parts[parts.length - 1];
-  // const newPath = path+'.'+ext;
-  // fs.renameSync(path, newPath);
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
 
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
-    const {title,summary} = req.body;
+    const {title,summary,content} = req.body;
     const postDoc = await Post.create({
       title,
       summary,
@@ -92,20 +84,37 @@ app.post('/post', async (req,res) => {
 
 });
 
-app.put('/post', async (req,res) => {
-  // let newPath = null;
-  // if (req.file) {
-  //   const {originalname,path} = req.file;
-  //   const parts = originalname.split('.');
-  //   const ext = parts[parts.length - 1];
-  //   newPath = path+'.'+ext;
-  //   fs.renameSync(path, newPath);
-  // }
+app.post('/comment', uploadMiddleware.single('file'), async (req,res) => {
 
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
-    const {id,title,summary} = req.body;
+    const {message} = req.body;
+    const CommentDoc = await Comment.create({
+      message,
+      // content,
+      // cover:newPath,
+      author:info.id,
+    });
+    res.json(CommentDoc);
+  });
+
+});
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
     const postDoc = await Post.findById(id);
     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
     if (!isAuthor) {
@@ -114,8 +123,8 @@ app.put('/post', async (req,res) => {
     await postDoc.update({
       title,
       summary,
-      // content,
-      // cover: newPath ? newPath : postDoc.cover,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
     });
 
     res.json(postDoc);
@@ -128,7 +137,7 @@ app.get('/post', async (req,res) => {
     await Post.find()
       .populate('author', ['username'])
       .sort({createdAt: -1})
-      .limit(20)
+      .limit(100)
   );
 });
 
@@ -139,4 +148,3 @@ app.get('/post/:id', async (req, res) => {
 })
 
 app.listen(4000);
-//
